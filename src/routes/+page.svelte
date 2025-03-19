ad3b890d352fb265ec7e934d24d2898206baca0d
joostf <j.faber@hva.nl>
2025-03-19 13:53:20 +0100

    feat: first setup project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+
+<section class="image-with-text">
+  <figure class="image-container">
+      <img src="/img/visualthinking.png" alt="Visual Thinking" class="rounded" />
+      <figcaption class="font-semibold">
+          <span class="small-body">Het Visual Thinking platform werd de afgelopen jaren door meerdere studenten, van verschillende opleidingen, ontwikkeld.</span>
+      </figcaption>
+  </figure>
+
+  <div class="text-container">
+      <h2 class="large-heading">FDND Pixel Parade</h2>
+
+      <p>Bij FDND werken we doorlopend aan langlopende projecten. De Pixel Parade is ene verzameling van ons beste werk.</p>
+  </div>
+</section>
